24b930f9aaefedf347243a41a428e5393b80d0c8
Ronan-Vallee <rvallee.pro@gmail.com>
2025-04-03 16:26:06 +0200

    feat(home): Add projects section to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import ContactSection from "@/components/sections/contact-section";
 import HeroSection from "@/components/sections/hero-section";
+import ProjectsSection from "@/components/sections/projects-section";
 
 export default function Home() {
 	return (
 		<>
 			<HeroSection />
+			<ProjectsSection />
 			<ContactSection />
 		</>
 	);
